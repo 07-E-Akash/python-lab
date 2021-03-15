f08cfe4d949af70bd20e0720d5392fe9929126a1
07-E-Akash <80275272+07-E-Akash@users.noreply.github.com>
2021-03-15 19:26:02 +0530

    alphabets, digit, specialcharacter

---
+++ char.py
@@ -0,0 +1,9 @@
+n = input()
+if n>= 'A' and n <='Z':
+  print("upper case alaphabet")
+elif n>= 'a' and n<='z':
+    print("lower case alphabet")
+elif n>= '0' and n<= '9':
+    print("digit")
+else:
+    print("special character")
